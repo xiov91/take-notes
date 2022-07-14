4449b9d58a9bdc7ddf0f4f7a95208b39d2ffdf52
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-07-14 11:53:14 -0400

    POST bugs fixed

--- server.js
+++ server.js
@@ -1,45 +1,43 @@
 const express = require('express');
-const { db } = require('./db/db');
+const db = require('./db/db.json');
+const {notes} = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
 // Sets up the port, so we aren't hardcoded into just one?
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-let notesArray = { db };
+// let noteArray = db;
 
-// this function SHOULD create a new note, write it with fs, then push it to the db.json file.
-// it's coming back undefined though. Insomnia let the POST request go through before this.
-// I think it's the notesArray, I had to improvise from the Zookeepr example and I think I got lost...
+console.log(db);
+console.log(notes);
 
-function createNewNote(body, notesArray) {
+function createNewNote(body, noteArray) {
     const note = body;
-    notesArray.push(db);
+    noteArray.push(note);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray}, null, 2)
+        JSON.stringify({ db: noteArray }, null, 2)
     );
-    return db;
-}
+
+    return note;
+} 
 
 // app.get to GET the info from the db.json
 app.get('/api/db', (req, res) => {
     res.json(db);
 })
 
-// app.post to create a new POST request
 app.post('/api/db', (req, res) => {
-    const note = createNewNote(req.body, db);
-
-    res.json(db);
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
 
 // the On switch!
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
