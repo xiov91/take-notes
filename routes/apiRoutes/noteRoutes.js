e6370501f71c5e60824385b045c52bfc085a7540
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-07-17 18:53:37 -0400

    trying to troubleshoot

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -3,13 +3,11 @@ const createNewNote = require('../../lib/note-functions');
 const db = require('../../db/db.json');
 const {notes} = require('../../db/db.json');
 
-// when this and only this is just /notes, it at least loads the unformatted json
-// also loads it when they're ALL /notes
-router.get('/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     res.json(db);
 })
 
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     const note = createNewNote(req.body, notes);
     res.json(note);
     }
