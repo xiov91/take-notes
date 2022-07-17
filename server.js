933bb0927704219b22b8700dc2f637f4e2c86ce1
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-07-17 18:35:07 -0400

    fixed htmlRoutes line

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes/noteRoutes');
-const htmlRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/index');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
